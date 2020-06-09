5bebde952655865a5c12d7aa6cde8852478cce36
Xanas1252 <younessanass86@gmail.com>
2020-06-09 20:18:28 +0100

    all

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
-	<link rel="stylesheet" href="outils/css/main.css">
+	<link rel="stylesheet" href="main.css">
 
 	<title>Accueil</title>
 
